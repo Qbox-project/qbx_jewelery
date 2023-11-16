68a919fd78cd258fb98c53c3d9d097b2f0c7ff6e
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 03:32:03 -0500

    fix: import modules

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ version '1.0.0'
 
 shared_scripts {
     '@ox_lib/init.lua',
-    '@qbx_core/import.lua',
+    '@qbx_core/modules/utils.lua',
     '@qbx_core/shared/locale.lua',
     'locale/en.lua',
     'locale/*.lua',
@@ -17,8 +17,6 @@ shared_scripts {
 client_script 'client.lua'
 
 server_script 'server.lua'
-
-modules {'qbx_core:utils'}
 
 lua54 'yes'
 use_experimental_fxv2_oal 'yes'
