cc415b07e1ae3640395cb8f0ee0e6da7c2d20fbb
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-12-13 21:53:50 -0500

    refactor: style fixes

--- server/main.lua
+++ server/main.lua
@@ -1,107 +1,143 @@
 local config = require 'config.server'
 local sharedConfig = require 'config.shared'
-local ElectricalBusy
-local StartedElectrical = {}
-local StartedCabinet = {}
-local AlarmFired
+local electricalBusy
+local startedElectrical = {}
+local startedVitrine = {}
+local alarmFired
+local ITEMS = exports.ox_inventory:Items()
 
 lib.callback.register('qb-jewelery:callback:electricalbox', function(source)
-    local Player = exports.qbx_core:GetPlayer(source)
-    local PlayerCoords = GetEntityCoords(GetPlayerPed(source))
-    local Amount = exports.qbx_core:GetDutyCountType('leo')
+    local player = exports.qbx_core:GetPlayer(source)
+    local playerCoords = GetEntityCoords(GetPlayerPed(source))
+    local amount = exports.qbx_core:GetDutyCountType('leo')
 
-    if ElectricalBusy then exports.qbx_core:Notify(source, Lang:t('notify.busy')) return end
-    if exports.ox_inventory:Search(source, 'count', sharedConfig.doorlock.requiredItem) == 0 then exports.qbx_core:Notify(source, Lang:t('notify.noitem', { item = exports.ox_inventory:Items()[sharedConfig.doorlock.RrequiredItem].label }), 'error') return end
-    if Amount < config.minimumPolice then if config.notEnoughPoliceNotify then exports.qbx_core:Notify(source, Lang:t('notify.nopolice', { Required = config.minimumPolice }), 'error') end return end
-    if #(PlayerCoords - vector3(config.electrical.x, config.electrical.y, config.electrical.z)) > 2 then return end
-    ElectricalBusy = true
-    StartedElectrical[source] = true
-    if sharedConfig.doorlock.loseItemOnUse then Player.Functions.RemoveItem(sharedConfig.doorlock.requiredItem) end
+    if electricalBusy then
+        exports.qbx_core:Notify(source, Lang:t('notify.busy'))
+        return
+    end
+
+    if exports.ox_inventory:Search(source, 'count', sharedConfig.doorlock.requiredItem) == 0 then
+        exports.qbx_core:Notify(source, Lang:t('notify.noitem', {item = ITEMS[sharedConfig.doorlock.RrequiredItem].label}), 'error')
+        return
+    end
+    if amount < config.minimumPolice then
+        if config.notEnoughPoliceNotify then
+            exports.qbx_core:Notify(source, Lang:t('notify.nopolice', {Required = config.minimumPolice}), 'error')
+        end
+        return
+    end
+    
+    if #(playerCoords - vector3(config.electrical.x, config.electrical.y, config.electrical.z)) > 2 then return end
+
+    electricalBusy = true
+    startedElectrical[source] = true
+
+    if sharedConfig.doorlock.loseItemOnUse then
+        player.Functions.RemoveItem(sharedConfig.doorlock.requiredItem)
+    end
+
     return true
 end)
 
-lib.callback.register('qb-jewelery:callback:cabinet', function(source, ClosestCabinet)
-    local PlayerPed = GetPlayerPed(source)
-    local PlayerCoords = GetEntityCoords(PlayerPed)
-    local AllPlayers = exports.qbx_core:GetQBPlayers()
+lib.callback.register('qb-jewelery:callback:cabinet', function(source, closestVitrine)
+    local playerPed = GetPlayerPed(source)
+    local playerCoords = GetEntityCoords(playerPed)
+    local allPlayers = exports.qbx_core:GetQBPlayers()
 
-    if #(PlayerCoords - sharedConfig.cabinets[ClosestCabinet].coords) > 1.8 then return end
-    if not config.allowedWeapons[GetSelectedPedWeapon(PlayerPed)] then exports.qbx_core:Notify(source, Lang:t('notify.noweapon')) return end
-    if sharedConfig.cabinets[ClosestCabinet].isBusy then exports.qbx_core:Notify(source, Lang:t('notify.busy')) return end
-    if sharedConfig.cabinets[ClosestCabinet].isOpened then exports.qbx_core:Notify(source, Lang:t('notify.cabinetdone')) return end
+    if #(playerCoords - sharedConfig.vitrines[closestVitrine].coords) > 1.8 then return end
 
-    StartedCabinet[source] = ClosestCabinet
-    sharedConfig.cabinets[ClosestCabinet].isBusy = true
-    for k in pairs(AllPlayers) do
+    if not config.allowedWeapons[GetSelectedPedWeapon(playerPed)] then
+        exports.qbx_core:Notify(source, Lang:t('notify.noweapon'))
+        return
+    end
+
+    if sharedConfig.vitrines[closestVitrine].isBusy then
+        exports.qbx_core:Notify(source, Lang:t('notify.busy'))
+        return
+    end
+
+    if sharedConfig.vitrines[closestVitrine].isOpened then
+        exports.qbx_core:Notify(source, Lang:t('notify.cabinetdone'))
+        return
+    end
+
+    startedVitrine[source] = closestVitrine
+    sharedConfig.vitrines[closestVitrine].isBusy = true
+    for k in pairs(allPlayers) do
         if k ~= source then
-            if #(GetEntityCoords(GetPlayerPed(k)) - sharedConfig.cabinets[ClosestCabinet].coords) < 20 then
-                TriggerClientEvent('qb-jewelery:client:synceffects', k, ClosestCabinet, source)
+            if #(GetEntityCoords(GetPlayerPed(k)) - sharedConfig.vitrines[closestVitrine].coords) < 20 then
+                TriggerClientEvent('qb-jewelery:client:synceffects', k, closestVitrine, source)
             end
         end
     end
     return true
 end)
 
-local function FireAlarm()
-    if AlarmFired then return end
+local function fireAlarm()
+    if alarmFired then return end
 
     TriggerEvent('police:server:policeAlert', Lang:t('notify.police'), 1, source)
     TriggerEvent('qb-scoreboard:server:SetActivityBusy', 'jewellery', true)
     TriggerClientEvent('qb-jewelery:client:alarm', -1)
-    AlarmFired = true
+    alarmFired = true
+
     SetTimeout(config.timeOut, function()
-        local DoorEntrance = exports.ox_doorlock:getDoorFromName(sharedConfig.doorlock.name)
-        TriggerEvent('ox_doorlock:setState', DoorEntrance.id, 1)
-        AlarmFired = false
+        local doorEntrance = exports.ox_doorlock:getDoorFromName(sharedConfig.doorlock.name)
+        TriggerEvent('ox_doorlock:setState', doorEntrance.id, 1)
+        alarmFired = false
         TriggerEvent('qb-scoreboard:server:SetActivityBusy', 'jewellery', false)
-        for i = 1, #sharedConfig.cabinets do
-            sharedConfig.cabinets[i].isOpened = false
+
+        for i = 1, #sharedConfig.vitrines do
+            sharedConfig.vitrines[i].isOpened = false
         end
-        TriggerClientEvent('qb-jewelery:client:syncconfig', -1, sharedConfig.cabinets)
+        
+        TriggerClientEvent('qb-jewelery:client:syncconfig', -1, sharedConfig.vitrines)
     end)
 end
 
 RegisterNetEvent('qb-jewelery:server:endcabinet', function()
-    local Player = exports.qbx_core:GetPlayer(source)
-    local PlayerCoords = GetEntityCoords(GetPlayerPed(source))
-    local ClosestCabinet = StartedCabinet[source]
+    local player = exports.qbx_core:GetPlayer(source)
+    local playerCoords = GetEntityCoords(GetPlayerPed(source))
+    local closestVitrine = startedVitrine[source]
 
-    if not ClosestCabinet then return end
-    if sharedConfig.cabinets[ClosestCabinet].isOpened then return end
-    if not sharedConfig.cabinets[ClosestCabinet].isBusy then return end
-    if #(PlayerCoords - sharedConfig.cabinets[ClosestCabinet].coords) > 1.8 then return end
+    if not closestVitrine then return end
+    if sharedConfig.vitrines[closestVitrine].isOpened then return end
+    if not sharedConfig.vitrines[closestVitrine].isBusy then return end
+    if #(playerCoords - sharedConfig.vitrines[closestVitrine].coords) > 1.8 then return end
 
-    sharedConfig.cabinets[ClosestCabinet].isOpened = true
-    sharedConfig.cabinets[ClosestCabinet].isBusy = false
-    StartedCabinet[source] = nil
+    sharedConfig.vitrines[closestVitrine].isOpened = true
+    sharedConfig.vitrines[closestVitrine].isBusy = false
+    startedVitrine[source] = nil
+
     for _ = 1, math.random(config.reward.minAmount, config.reward.maxAmount) do
         local RandomItem = config.reward.items[math.random(1, #config.reward.items)]
-        Player.Functions.AddItem(RandomItem.name, math.random(RandomItem.min, RandomItem.max))
+        player.Functions.AddItem(RandomItem.name, math.random(RandomItem.min, RandomItem.max))
     end
-    TriggerClientEvent('qb-jewelery:client:syncconfig', -1, sharedConfig.cabinets)
-    FireAlarm()
+
+    TriggerClientEvent('qb-jewelery:client:syncconfig', -1, sharedConfig.vitrines)
+    fireAlarm()
 end)
 
 RegisterNetEvent('qb-jewellery:server:failedhackdoor', function()
-    ElectricalBusy = false
-    StartedElectrical[source] = false
+    electricalBusy = false
+    startedElectrical[source] = false
     exports.qbx_core:Notify(source, 'Hack failed', 'error')
 end)
 
 RegisterNetEvent('qb-jewellery:server:succeshackdoor', function()
-    local DoorEntrance = exports.ox_doorlock:getDoorFromName(sharedConfig.doorlock.name)
-    local PlayerCoords = GetEntityCoords(GetPlayerPed(source))
+    local doorEntrance = exports.ox_doorlock:getDoorFromName(sharedConfig.doorlock.name)
+    local playerCoords = GetEntityCoords(GetPlayerPed(source))
 
-    if not ElectricalBusy then return end
-    if not StartedElectrical[source] then return end
-    if #(PlayerCoords - vector3(config.electrical.x, config.electrical.y, config.electrical.z)) > 2 then return end
+    if not electricalBusy then return end
+    if not startedElectrical[source] then return end
+    if #(playerCoords - vector3(config.electrical.x, config.electrical.y, config.electrical.z)) > 2 then return end
 
-    ElectricalBusy = false
-    StartedElectrical[source] = false
+    electricalBusy = false
+    startedElectrical[source] = false
     exports.qbx_core:Notify(source, 'Hack successful')
-    TriggerEvent('ox_doorlock:setState', DoorEntrance.id, 0)
+    TriggerEvent('ox_doorlock:setState', doorEntrance.id, 0)
 end)
 
 AddEventHandler('playerJoining', function(source)
-    TriggerClientEvent('qb-jewelery:client:syncconfig', source, sharedConfig.cabinets)
+    TriggerClientEvent('qb-jewelery:client:syncconfig', source, sharedConfig.vitrines)
 end)
