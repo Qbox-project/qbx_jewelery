aa36caebcef5dca0aebc1848cac49a36dadd15da
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-01-03 10:02:31 -0500

    refactor(client): dropFingerprint

--- config/client.lua
+++ config/client.lua
@@ -1,5 +1,5 @@
 return {
-    fingerprintDropChance = 60,
+    fingerprintChance = 50, -- Chance of dropping a fingerprint if not wearing gloves
     useDrawText = false,
     useTarget = false,
     alarmDuration = 240000,
